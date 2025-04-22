fc2857144e75a97226ea48345474374bf206602c
Maksim Shapaev <shapaev.maxim@gmail.com>
2025-04-22 09:48:45 +0300

    initial commit

---
+++ src/Components/CardsView/CardsView.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+import {ShopCard} from '../ShopCard/ShopCard'
+
+export const CardsView = (props) => {
+    return (
+        <ul className='cards-module'>
+            {props.cards.map((card, i) => <ShopCard card={card} key={i}/>)}
+        </ul>
+    )
+}
---
+++ src/Components/ListView/ListView.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+import {ShopItem} from '../ShopItem/ShopItem'
+
+export const ListView = (props) => {
+    return (
+        <ul className='cards-list'>
+            {props.items.map((item, i) => <ShopItem item={item} key={i}/>)}
+        </ul>
+    )
+}
